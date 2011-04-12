7b1cea6850ee3734f667025310b886eaf50f1de8
masato <masato.t@gmail.com>
2011-04-13 02:12:52 +0900

    first commit

---
+++ HelloWorldCUI/HelloWorld.h
@@ -0,0 +1,16 @@
+//
+//  HelloWorld.h
+//  HelloWorldCUI
+//
+//  Created by MasatoTsukagoshi on 11/04/13.
+//  Copyright 2011 Private. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface HelloWorld : NSObject {
+    
+}
+
+@end
---
+++ HelloWorldCUI/HelloWorld.m
@@ -0,0 +1,14 @@
+//
+//  HelloWorld.m
+//  HelloWorldCUI
+//
+//  Created by MasatoTsukagoshi on 11/04/13.
+//  Copyright 2011 Private. All rights reserved.
+//
+
+#import "HelloWorld.h"
+
+
+@implementation HelloWorld
+
+@end
