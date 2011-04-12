8be6e236e671e732aaad0c5ed966f865a5e854c1
masato <masato.t@gmail.com>
2011-04-13 02:21:24 +0900

    HelloWorld

--- HelloWorldCUI/HelloWorld.m
+++ HelloWorldCUI/HelloWorld.m
@@ -11,4 +11,15 @@
 
 @implementation HelloWorld
 
+- (void) hello{
+    printf("Hello World.\n");
+}
+
 @end
+
+int main(int argc, char **argv){
+    id obj = [HelloWorld alloc];
+    [obj hello];
+    
+    return 0;
+}
