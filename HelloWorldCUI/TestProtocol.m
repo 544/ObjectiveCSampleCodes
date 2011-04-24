a516f42c0daf1266d059d4ff4d3f0cd66826f414
masato <masato.t@gmail.com>
2011-04-24 20:50:44 +0900

    プロトコルに合致しないものの検査ロジック追記

--- HelloWorldCUI/TestProtocol.m
+++ HelloWorldCUI/TestProtocol.m
@@ -20,10 +20,10 @@
     [array addRealNumber:@"0.2"];
     
     // NSMutableArrayの中身がすべてプロトコルを満たしているか判断
-    BOOL boolean = NO;
+    BOOL boolean = YES;
     for (id e in array) {
-        if ([e conformsToProtocol:@protocol(RealNumber)]) {
-            boolean = YES;
+        if (![e conformsToProtocol:@protocol(RealNumber)]) {
+            boolean = NO;
         }
     }
     
@@ -38,22 +38,29 @@
 		NSLog(@">> %@", e);
 	}
     
+    NSLog(@"---------");
+    // RealNumberプロトコルでないものを突っ込んでみる。
+    NSNumber *num = [[NSNumber alloc] initWithDouble:1.9];
+    [array addObject:num];
+    
+    //再検査
+    boolean = YES;
+    for (id e in array) {
+        if (![e conformsToProtocol:@protocol(RealNumber)]) {
+            boolean = NO;
+        }
+    }
+    if (boolean) {
+        NSLog(@"ArraysMember is RealNumberProtocol");
+        [array sort];
+    } else{
+        NSLog(@"ArraysMember is not RealNumberProtocol");
+    }
+    
+    for (id e in array) {
+		NSLog(@">> %@", e);
+	}
 
-//    NSMutableArray *array2 = [[NSMutableArray alloc] init];
-//
-//    [array2 addRealNumber:[@"1.3" realValue]];
-//    [array2 addRealNumber:[@"2" realValue]];
-//    [array2 addRealNumber:[@"0.2" realValue]];
-//
-//    if ([array2 conformsToProtocol:@protocol(RealNumber)]) {
-//        NSLog(@"isRealNumberProtocol");
-//        [array2 sort];
-//    } else
-//        NSLog(@"isNotRealNumberProtocol");
-//    
-//    for (id e in array2) {
-//		NSLog(@">> %@", e);
-//	}
 
 
 }
